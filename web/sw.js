64d13fb1f84ec3ef32df02b15c753ba8651686bf
boyancik-art <boyancik@gmail.com>
2026-09-01 13:53:54 +0300

    Rotate PWA cache for v40.5

--- web/sw.js
+++ web/sw.js
@@ -1,5 +1,5 @@
-const CACHE='transport-report-ts-v40-4';
-const ASSETS=['./manifest.webmanifest?v=404','./icon-192.png','./icon-512.png'];
+const CACHE='transport-report-ts-v40-5';
+const ASSETS=['./manifest.webmanifest?v=405','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const req=e.request;e.respondWith(fetch(req,{cache:'no-store'}).then(r=>{if(r.ok&&new URL(req.url).origin===location.origin){const copy=r.clone();caches.open(CACHE).then(c=>c.put(req,copy))}return r}).catch(()=>caches.match(req)))})
